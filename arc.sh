12120aa1bd2484d56d05077bb6010d5f0b3f0b74
Nathan Weizenbaum <nex342@gmail.com>
2008-01-31 22:27:21 -0800

    Make sure rlwrap exists before we try to use it.

--- arc.sh
+++ arc.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 ARC_DIR=~/src/arc
-rlwrap -C arc mzscheme -m -d "$ARC_DIR/as.scm"
+
+if which rlwrap &> /dev/null; then
+    rlwrap -C arc mzscheme -m -d "$ARC_DIR/as.scm"
+else
+    mzscheme -m -d "$ARC_DIR/as.scm"
+fi
