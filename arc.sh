8b0405e9f12b9ce36930d0f0af8b41dbb96f4d71
Nathan Weizenbaum <nex342@gmail.com>
2008-02-01 00:32:38 -0800

    Get arc.sh to be much smarter about figuring out where as.scm and friends are. Patch by manveru.

--- arc.sh
+++ arc.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-ARC_DIR=~/src/arc
+
+if [ -L "$0" ]; then
+  arc_dir=`readlink $0`
+else
+  arc_dir=$0
+fi
+
+ARC_DIR=`dirname $arc_dir`
 
 if which rlwrap &> /dev/null; then
-    rlwrap -C arc mzscheme -m -d "$ARC_DIR/as.scm"
+  rlwrap -C arc mzscheme -m -d "$ARC_DIR/as.scm"
 else
-    mzscheme -m -d "$ARC_DIR/as.scm"
+  mzscheme -m -d "$ARC_DIR/as.scm"
 fi
