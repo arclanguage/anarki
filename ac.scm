a68afd08329e5c25829e25668b38f206f5b1addb
Nathan Weizenbaum <nex342@gmail.com>
2008-02-01 22:11:34 -0800

    Control-D kills the arc prompt.

--- ac.scm
+++ ac.scm
@@ -69,6 +69,7 @@
         ; this line could be removed without changing semantics
         ((eq? (xcar (xcar s)) 'compose) (ac (decompose (cdar s) (cdr s)) env))
         ((pair? s) (ac-call (car s) (cdr s) env))
+        ((eof-object? s) (exit))
         (#t (err "Bad object in expression" s))))
 
 (define (literal? x)
