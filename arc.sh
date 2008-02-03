a9ade3c13cb2c27a0e0b053c6d13ead3042a3176
Nathan Weizenbaum <nex342@gmail.com>
2008-02-02 22:34:58 -0800

    Getting rid of the MzScheme bootstrapping in arc.sh. That's not really the right place, and it was the last remaining incompatibility point with the Git Anarki.

--- arc.sh
+++ arc.sh
@@ -7,12 +7,7 @@ else
 fi
 arc_dir=`dirname $arc_dir`
 
-mzscheme_dir=$arc_dir/../third-party/mzscheme
-mzscheme=$mzscheme_dir/bin/mzscheme
-if [ ! -e $mzscheme ]; then
-  cd $mzscheme_dir
-  ./configure && make && make install
-fi
+mzscheme=mzscheme
 
 if [ "$(type -p rlwrap)" ]; then
   rlwrap -C arc $mzscheme -m -d "$arc_dir/as.scm"
