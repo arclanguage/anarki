4bf951197d7a1cedb809c7c3facbe59207593eae
Eric Hanchrow <offby1@blarg.net>
2008-02-26 21:41:05 -0800

    arc.sh: bit more refactoring

--- arc.sh
+++ arc.sh
@@ -7,10 +7,8 @@ else
 fi
 arc_dir=`dirname $arc_dir`
 
-mzscheme="mzscheme --no-init-file --mute-banner --load-cd"
-
 if [ "$(type -p rlwrap)" -a ! "$1" = "--no-rl" ]; then
-  rlwrap -C arc $mzscheme "$arc_dir/as.scm"
-else
-  $mzscheme "$arc_dir/as.scm"
+  rl="rlwrap -C arc"
 fi
+
+$rl mzscheme --no-init-file --mute-banner --load-cd  "$arc_dir/as.scm"
