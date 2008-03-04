f292db17163c1dcc8dac210e8a68abd82f2a0d2b
Shlomif <shlomif@verdi.(none)>
2008-03-04 18:52:29 +0100

    Fixed Test.sh in certain cases.
    Fixed Test.sh when AlternateInterpreters is not present in the Test::Run
    list of plugins.

--- Test.sh
+++ Test.sh
@@ -6,7 +6,13 @@
 # * http://web-cpan.berlios.de/modules/Test-Run/
 # 
 export HARNESS_ALT_INTRP_FILE="$(pwd)/t/files/interpreters.conf.yml"
-if echo "$HARNESS_PLUGINS" | grep -q -E '(^| )AlternateInterpeters( |$)' ; then
-    export HARNESS_PLUGINS="$HARNESS_PLUGINS AlternateInterpeters"
+plugin="AlternateInterpreters"
+if ! echo "$HARNESS_PLUGINS" | grep -q -E '(^| )'"$plugin"'( |$)' ; then
+    if test -z "$HARNESS_PLUGINS" ; then
+        export HARNESS_PLUGINS="$plugin"
+    else
+        export HARNESS_PLUGINS="$HARNESS_PLUGINS $plugin"
+    fi
 fi
+# echo "$HARNESS_PLUGINS"
 runprove t/*.t
