da1a7fc06f580f5a58df953631419ea3bc0c973d
Michael Arntzenius <daekharel@gmail.com>
2009-08-02 01:39:50 +0200

    add arc.sh

---
+++ arc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+arc_dir=$0
+
+if [ -L "$0" ]; then
+  arc_dir=$(readlink "$0")
+fi
+
+arc_dir=$(dirname "$arc_dir")
+
+if [ "$(type -p rlwrap)" -a ! "$1" = "--no-rl" ]; then
+  rl="rlwrap -C arc"
+fi
+
+$rl mzscheme --no-init-file --mute-banner --load-cd  "$arc_dir/as.scm"
