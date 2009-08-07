000c9a1d5bddfe72bc88db8a72492bea8c24e4a9
Michael Arntzenius <daekharel@gmail.com>
2009-08-08 01:10:44 +0200

    update arc.sh to work with mzscheme v4

--- arc.sh
+++ arc.sh
@@ -7,9 +7,14 @@ if [ -L "$0" ]; then
 fi
 
 arc_dir=$(dirname "$arc_dir")
+cd $arc_dir
 
 if [ "$(type -p rlwrap)" -a ! "$1" = "--no-rl" ]; then
   rl="rlwrap -C arc"
 fi
 
-$rl mzscheme --no-init-file --mute-banner --load-cd  "$arc_dir/as.scm"
+case "$(mzscheme --version)" in
+  *v4.*) # AFAICT, there is no equivalent of --mute-banner for mzscheme v4
+         $rl mzscheme -i --no-init-file --load as.scm;;
+  *) $rl mzscheme --no-init-file --mute-banner --load as.scm;;
+esac
