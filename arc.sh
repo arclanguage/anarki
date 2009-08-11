eb55979588bb01d19cb5b47c6c0bb3ddf872b8ee
Michael Arntzenius <daekharel@gmail.com>
2009-08-11 00:15:31 -0400

    don't display repl when stdin is non-interactive

--- arc.sh
+++ arc.sh
@@ -13,8 +13,27 @@ if [ "$(type -p rlwrap)" -a ! "$1" = "--no-rl" ]; then
   rl="rlwrap -C arc"
 fi
 
+# I wish there were some cleaner way to do this
 case "$(mzscheme --version)" in
-  *v4.*) # AFAICT, there is no equivalent of --mute-banner for mzscheme v4
-         $rl mzscheme --repl --no-init-file --load as.scm;;
-  *) $rl mzscheme --no-init-file --mute-banner --load as.scm;;
+    *v4.*) plt4=yes;;
+    *) plt4=no;;
 esac
+
+opts="--no-init-file"
+
+# It seems like there ought to be a program for determining whether standard
+# input is a tty, but I can't find one; so we use mzscheme instead.
+if [ $plt4 = yes ]; then
+    case $(mzscheme -e '(terminal-port? (current-input-port))') in
+        '#t') # AFAICT, there is no equivalent of --mute-banner for mzscheme v4
+              opts+=" --repl --load";;
+        *) opts+=" --script";;
+    esac
+else
+    case $(mzscheme -mve '(display (terminal-port? (current-input-port)))') in
+        '#t') opts+=" --mute-banner --load";;
+        *) opts+=" --script";;
+    esac
+fi
+
+$rl mzscheme $opts as.scm $@
