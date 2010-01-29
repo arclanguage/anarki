c931a1d687f7855568f58c317aa57d475c168017
Kartik K. Agaram <vc@akkartik.com>
2010-01-28 16:03:31 -0800

    http://awwx.ws/defarc - define scheme primitives to be redefinable in arc

--- ac.scm
+++ ac.scm
@@ -9,6 +9,23 @@
 (require (lib "foreign.ss"))
 (unsafe!)
 
+(define (ac-global-name s)
+  (string->symbol (string-append "_" (symbol->string s))))
+
+(define-syntax defarc
+  (syntax-rules ()
+    ((defarc (name . args) body ...)
+     (defarc name (name . args) body ...))
+    ((defarc arc-name (scheme-name . args) body ...)
+     (begin
+       (xdef arc-name (lambda args body ...))
+       (defarc arc-name scheme-name)))
+    ((defarc arc-name scheme-name)
+     (define (scheme-name . args)
+       (apply (namespace-variable-value (ac-global-name 'arc-name)) args)))
+    ((defarc name)
+     (defarc name name))))
+
 ; compile an Arc expression into a Scheme expression,
 ; both represented as s-expressions.
 ; env is a list of lexically bound variables, which we
@@ -207,9 +224,6 @@
                  (cons (car source) token)
                  acc
                  keepsep?))))
-
-(define (ac-global-name s)
-  (string->symbol (string-append "_" (symbol->string s))))
 
 (define (ac-var-ref s env)
   (if (lex? s env)
