b2c6263b4012199c8dd6a1cef66c14a2e93fd534
hasen j <hasan.aljudy@gmail.com>
2010-10-09 20:32:35 -0600

    fix rlwrap word boundaries

--- arc.sh
+++ arc.sh
@@ -5,7 +5,7 @@ arc_dir=$(dirname "$(readlink "$0")")
 if [ "$1" = "--no-rl" ]; then
     shift
 elif [ "$(type rlwrap)" ]; then
-    rl='rlwrap --complete-filenames --quote-character "\"" --remember --break-chars "[]()\"" -C arc'
+    rl='rlwrap --complete-filenames --quote-character "\"" --remember --break-chars "[]()!:~\"" -C arc'
 fi
 
 # I wish there were some cleaner way to do this
