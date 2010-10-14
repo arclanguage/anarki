b63514180cdf840b760776c0a92efda6dd0745b6
Ross Angle <rokitna@hotmail.com>
2010-10-14 16:09:24 -0700

    Fixed missing setuid bug on Windows.
    Amusingly, it's been exactly 365 days since Ken Shirriff's original bug report.

--- ac.scm
+++ ac.scm
@@ -1020,7 +1020,11 @@
 
 ; allow Arc to give up root privileges after it
 ; calls open-socket. thanks, Eli!
-(define setuid (get-ffi-obj 'setuid #f (_fun _int -> _int)))
+(define setuid (get-ffi-obj 'setuid #f (_fun _int -> _int)
+                 ; If we're on Windows, there is no setuid, so we make
+                 ; a dummy version. See "Arc 3.1 setuid problem on
+                 ; Windows," http://arclanguage.org/item?id=10625.
+                 (lambda () (lambda (x) 'nil))))
 (xdef setuid setuid)
 
 (xdef new-thread thread)
