5fb32f96cbd6715a8ff5d42483bd7414528fb317
Brandon Bloom <brandon@brandonbloom.name>
2011-09-03 21:02:26 -0700

    Support for tpope/vim-pathogen in extras/vim/Install.sh

--- extras/vim/Install.sh
+++ extras/vim/Install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+install_to="$HOME/.vim"
+if [ -d "$install_to/bundle" ]; then
+  # See https://github.com/tpope/vim-pathogen
+  install_to="$install_to/bundle/arc"
+fi
 for I in ftplugin indent syntax ; do
-    target="$HOME/.vim/$I"
+    target="$install_to/$I"
     mkdir -p "$target"
     cp -f "$I/arc.vim" "$target/";
 done
