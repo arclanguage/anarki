af41fae85fe68b1f6c76ff3653ab8cb495a6a6f6
Kartik K. Agaram <vc@akkartik.com>
2013-02-20 13:24:57 -0800

    no, undo recent changes to print
    They were buggy, printing numbers differently inside lists, etc.
    http://arclanguage.org/item?id=17357

--- ac.scm
+++ ac.scm
@@ -944,14 +944,8 @@
       (flush-output port)))
   'nil)
 
-(define (ar-display arg port)
-  (display (if (eq? 'num (ar-type arg))
-             (exact->inexact arg)
-             arg)
-           port))
-
 (xdef swrite (lambda args (printwith write args)))
-(xdef disp  (lambda args (printwith ar-display args)))
+(xdef disp  (lambda args (printwith display args)))
 
 ; sread = scheme read. eventually replace by writing read
 
