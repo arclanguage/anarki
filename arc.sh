f9624f6d9991c9613d378fa02cc0df86f0763459
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-02-21 03:21:19 +0400

    Use 'greadlink' instead of non gnu 'readlink' when running on OSX.

--- arc.sh
+++ arc.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-arc_dir=$(dirname "$(readlink -f "$0")")
+
+if [ $(uname) == "Darwin" ]; then
+    if which -s greadlink; then
+        arc_dir=$(dirname "$(greadlink -f "$0")")
+    else
+        echo 'Please do "brew install coreutils".'
+        exit 1
+    fi
+else
+    arc_dir=$(dirname "$(readlink -f "$0")")
+fi
 
 if [ "$1" = "--no-rl" ]; then
     shift
