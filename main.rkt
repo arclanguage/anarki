8e75c9a2dd66c6c62f462aa788de6c7ce2d99191
Ross Angle <rokitna@hotmail.com>
2016-03-12 23:40:06 -0800

    Exported more useful things from (require anarki). Now Anarki loads lazily, but (anarki-init) can be used to force Anarki to load.

--- main.rkt
+++ main.rkt
@@ -4,6 +4,22 @@
 
 (define-runtime-path boot-scm-path "boot.scm")
 
-(define tl (dynamic-require boot-scm-path 'tl))
+; Loading Anarki is expensive, so we do it on demand, and the
+; (anarki-init) function is dedicated to this. Fortunately, we pay the
+; initialization cost only once even though we're loading
+; `dynamic-require` multiple times.
 
-(provide tl)
+(define (anarki-init)
+  ; We load the export, but then we ignore it. We could use any export
+  ; for this.
+  (dynamic-require boot-scm-path 'tl)
+  (void))
+
+(define-values (anarki-repl anarki-load anarki-namespace)
+  (apply values
+    (map (lambda (var)
+           (lambda args
+             (apply (dynamic-require boot-scm-path var) args)))
+      '(tl aload main-namespace))))
+
+(provide (all-defined-out))
