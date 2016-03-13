85aaa4ad0215d71d22900874cbf6b21eb104b225
Ross Angle <rokitna@hotmail.com>
2016-03-13 00:10:10 -0800

    Added some (require anarki) exports for getting global variables and accessing Anarki libraries.

--- main.rkt
+++ main.rkt
@@ -1,12 +1,14 @@
-#lang racket/base
+#lang racket
 
 (require racket/runtime-path)
 
 (define-runtime-path boot-scm-path "boot.scm")
 
+(define anarki-path (path-only boot-scm-path))
+
 ; Loading Anarki is expensive, so we do it on demand, and the
-; (anarki-init) function is dedicated to this. Fortunately, we pay the
-; initialization cost only once even though we're loading
+; (anarki-init) function is dedicated to doing so. Fortunately, we pay
+; the initialization cost only once even though we're loading
 ; `dynamic-require` multiple times.
 
 (define (anarki-init)
@@ -15,11 +17,12 @@
   (dynamic-require boot-scm-path 'tl)
   (void))
 
-(define-values (anarki-repl anarki-load anarki-namespace)
+(define-values
+  (anarki-repl anarki-load anarki-namespace anarki-global-name)
   (apply values
     (map (lambda (var)
            (lambda args
              (apply (dynamic-require boot-scm-path var) args)))
-      '(tl aload main-namespace))))
+      '(tl aload main-namespace ac-global-name))))
 
-(provide (all-defined-out))
+(provide (except-out (all-defined-out) boot-scm-path))
