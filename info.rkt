22763833d73fc458b355c87c178ec64375b7f2ac
Ross Angle <rokitna@hotmail.com>
2016-03-14 21:50:29 -0700

    Declared dependencies on the specific things listed in the pkgs.racket-lang.org undeclared dependency report rather than guessing.

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "anarki")
-(define deps '("racket"))
+(define deps '("base" "compatibility-lib" "scheme-lib"))
 (define pkg-desc "Community-managed variant of the Arc dialect of Lisp")
 (define version "0.0")
