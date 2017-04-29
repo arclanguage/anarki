f9548c50ea6e7a9600c90786dbed90daa3ae1e26
Ross Angle <rokitna@hotmail.com>
2017-04-28 20:05:15 -0700

    add square brackets to the #lang

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -1,2 +1,8 @@
 #lang s-exp syntax/module-reader
 anarki/lang/module-begin
+#:read brackets-read
+#:read-syntax brackets-read-syntax
+
+(require (only-in "../brackets.scm"
+           [read brackets-read]
+           [read-syntax brackets-read-syntax]))
