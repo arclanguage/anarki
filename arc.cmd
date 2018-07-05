9cac70b67d217939017ea9227fd86c77f01f424e
Kartik Agaram <vc@akkartik.com>
2018-07-05 10:16:18 -0700

    Handle paths with spaces on Windows.
    Thanks jsgrahamus for the report, and for testing out this fix:
    http://arclanguage.org/item?id=20417

--- arc.cmd
+++ arc.cmd
@@ -3,6 +3,6 @@ SETLOCAL ENABLEEXTENSIONS
 REM SET me=%~n0
 SET arc_dir=%~dp0
 
-racket -t %arc_dir%boot.rkt -e "(anarki-windows-cli)" -- %*
+racket -t "%arc_dir%boot.rkt" -e "(anarki-windows-cli)" -- %*
 
 ENDLOCAL
