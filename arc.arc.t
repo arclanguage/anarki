arclanguage/anarki

commit 95ff09372158bb1590a1aad3992013668713ec57

Rename destructive append operation.
Also add a docstring, and return the appended list in the tradition of
Common Lisp's `nconc`.

Fixes #71.

arc.arc
@@ -744,6 +744,11 @@ append to 'xs'."
     (lastcons cdr.xs)
     xs))
 
+(def njoin (a b)
+"Concatenates/appends second argument to first, then returns first argument."
+  (ret a
+    (= (cdr lastcons.a) b)))
+
 ; Generalization of pair: (tuples x) = (pair x)
 
 (def tuples (xs (o n 2))

lib/spliceable-list.arc
@@ -1,11 +1,9 @@
-;; append elems to it, check n-elem suffix for a match, then splice the suffix out
-
 (def spliceable-list (n (o init))
 "Create a spliceable list with a constant suffix length of n. A spliceable
 list is a special data structure that efficiently supports the following
 operations:
 
-1. Appending items using [[append]].
+1. Destructively appending items using [[njoin]].
 2. Returning the last n items appended using [[suffix]] if there are at least
 n items to return.
 3. Dropping the last n items appended, and returning everything else. [[splice]]"
@@ -21,10 +19,7 @@ n items to return.
 (defextend empty (l) (isa l 'spliceable-list)
   (empty rep.l!contents))
 
-(def append (a b)
-  (= (cdr lastcons.a) b))
-
-(defextend append (l tail) (isa l 'spliceable-list)
+(defextend njoin (l tail) (isa l 'spliceable-list)
   (if empty.l
     (= rep.l!contents tail
        rep.l!last rep.tail)

lib/spliceable-list.arc.t
@@ -3,7 +3,7 @@
               two-elements   (spliceable-list 2 '(1 2))
               three-elements (spliceable-list 2 '(1 2 3))
               four-elements  (ret temp-three-elements (spliceable-list 2 '(1 2 3))
-                               (append temp-three-elements '(4))))
+                               (njoin temp-three-elements '(4))))
        (test initializes-without-list
              (assert-same (obj 
                             contents
@@ -40,12 +40,12 @@
                             nil
                             suffix-len
                             3)
-                          (do (append one-element (list 2))
+                          (do (njoin one-element (list 2))
                               (rep one-element))))
        (test suffix-3
              (assert-same '(2 3)
-                          (do (append one-element (list 2))
-                              (append one-element (list 3))
+                          (do (njoin one-element (list 2))
+                              (njoin one-element (list 3))
                               (suffix one-element))))
        (test splicing-without-suffix
              (assert-same '(1) (splice three-elements)))

lib/srv.arc
@@ -340,7 +340,7 @@
   (= pat (map int (coerce pat 'cons)))
   (let buffer (spliceable-list len.pat)
     (until (iso pat suffix.buffer)
-      (append buffer (list readb.in)))
+      (njoin buffer (list readb.in)))
     splice.buffer))
 
 ; convert list of bytes to string

commit e8f97466ee33bccf9cd43adac5547d43ffca11d0

Fix breakage in `nappend`
It had been broken since commit 95ff0937 in Nov 2017, when I moved it from
a library into arc.arc. Unfortunately I moved it to the wrong place, before
assignment was defined. Not only did it break `nappend`, it also yielded
cryptic error messages when attempting to use `nappend`.

`nappend` was originally introduced because I wanted it for spliceable
lists. Unfortunately I never wrote tests for it running on regular lists,
hiding the bug for over a year. Now starting to add tests.

Thanks Kinnard Hockenhull for reporting this issue.

arc.arc
@@ -744,23 +744,6 @@ Generalizes [[map1]] to functions with more than one argument."
   (nthcdr 10 '(1 2 3))
   nil)
 
-(def lastcons (xs)
-"Returns the absolute last link of list 'xs'. Save this value to efficiently
-append to 'xs'."
-  (if cdr.xs
-    (lastcons cdr.xs)
-    xs))
-
-(def njoin (a b)
-"Concatenates/appends second list to first, then returns entire result."
-  (ret a
-    (= (cdr lastcons.a) b)))
-
-(def nappend (l item)
-"Appends item to list, then returns list."
-  (ret l
-    (= (cdr lastcons.l) list.item)))
-
 ; Generalization of pair: (tuples x) = (pair x)
 
 (def tuples (xs (o n 2))
@@ -797,6 +780,8 @@ append to 'xs'."
   (disp (+ "Warning: " msg ". "))
   (map [do (write _) (disp " ")] args)
   (disp #\newline))
+
+;; more powerful assignment to arbitrary places
 
 (mac atomic body
 "Runs expressions in 'body' with exclusive access to system resources.
@@ -961,6 +946,23 @@ See [[atomic]]."
   _
   (= x 2 y 4)
   _)
+
+(def lastcons (xs)
+"Returns the absolute last link of list 'xs'. Save this value to efficiently
+append to 'xs'."
+  (if cdr.xs
+    (lastcons cdr.xs)
+    xs))
+
+(def njoin (a b)
+"Concatenates/appends second list to first, then returns entire result."
+  (ret a
+    (= (cdr lastcons.a) b)))
+
+(def nappend (l item)
+"Appends item to list, then returns list."
+  (ret l
+    (= (cdr lastcons.l) list.item)))
 
 ; http://arclanguage.org/item?id=12229
 (mac for (var init test update . body)

arc.arc.t
@@ -235,6 +235,12 @@
                                      '(3 4)
                                      (tree '((1 2) (5 6)))))))
 
+(suite nappend
+       (test destructive
+             (let x '(1 2)
+               (nappend x 3)
+               (assert-same '(1 2 3) x))))
+
 (suite cut
        (test finds-element-in-list
              (assert-same '(3 4 5) (cut '(1 2 3 4 5) 2)))