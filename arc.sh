7f2953ac3e4618c799e48739feac65fabc77c44e
Kartik K. Agaram <vc@akkartik.com>
2021-10-08 05:51:26 -0700

    remove Racket install instructions from arc.sh
    Since we rely on a fairly up-to-date Racket these days, using the OS
    installer no longer suffices. The current Ubuntu LTS (20.04) ships with
    Racket 7.2.

--- arc.sh
+++ arc.sh
@@ -63,16 +63,17 @@ EXAMPLES
 fi
 
 # try to suggest solutions to the most common issues people run into
+if ! which racket >&/dev/null
+then
+  echo "Please install racket by following the instructions in README.markdown."
+  exit 1
+fi
+
 case $(uname) in
     "Darwin")
         if ! which greadlink >&/dev/null
         then
           echo 'Please do "brew install coreutils."'
-          exit 1
-        fi
-        if ! which racket >&/dev/null
-        then
-          echo 'Please run "brew install racket && raco pkg install --auto drracket"'
           exit 1
         fi
         arc_dir=$(dirname "$(greadlink -f "$0")")
@@ -85,12 +86,6 @@ case $(uname) in
         ;;
 
     "Linux")
-        if ! which racket >&/dev/null
-        then
-          echo "Please install racket with your OS's package manager (apt-get, dpkg, rpm, yum, pacman, etc.)"
-          echo "Or go to http://racket-lang.org for more detailed instructions."
-          exit 1
-        fi
         arc_dir=$(dirname "$(readlink -f "$0")")
         if [[ $RLWRAP == true ]] && ! which rlwrap >&/dev/null
         then
